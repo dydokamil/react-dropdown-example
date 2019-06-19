35f79988ab70244adbfe3cbf3f101e1780698461
Kamil Dydo <dydokamil@gmail.com>
2019-06-19 20:54:16 +0200

    Added examples

---
+++ src/Card.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./Card.css";
+import CodeIcon from "./CodeIcon";
+
+function Card({ children, code }) {
+  const [isCodeShown, setIsCodeShown] = useState(false);
+
+  return (
+    <div className="card">
+      {children}
+      <div className="code-button-wrapper">
+        <button
+          className="code-toggle-button"
+          onClick={() => setIsCodeShown(curr => !curr)}
+        >
+          <CodeIcon width="20px" height="20px" />
+        </button>
+      </div>
+      {isCodeShown && (
+        <div className="code-big-wrapper">
+          <div className="code">
+            {code.map(line => (
+              <p className="code-line">{line}</p>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Card;
